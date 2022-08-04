2576e66a52c9556b607043c532dc9abc54a87ad5
Coen Steuns <c.steuns@gmail.com>
2022-08-04 12:42:02 +0200

    basic player movement and pixel perfect camera

---
+++ Assets/Scripts/Player/Movement/PlayerWaling.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerWaling : MonoBehaviour
+{
+
+    
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rb;
+    
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        var direction = Vector2.zero;
+        if(Input.GetKey(KeyCode.W)){
+            direction += Vector2.up;
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            direction += Vector2.down;
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            direction += Vector2.left;
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            direction += Vector2.right;
+        }
+        direction.Normalize();
+
+        rb.velocity = direction * speed;
+    }
+
+}
