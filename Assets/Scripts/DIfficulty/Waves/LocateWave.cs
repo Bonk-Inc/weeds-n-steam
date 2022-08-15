a2854f39a16cce1d4f296283ca8254109309792f
jvdwijk <joeyvdwijk@gmail.com>
2022-08-15 13:22:05 +0200

    location wave

--- Assets/Scripts/DIfficulty/Waves/LocateWave.cs
+++ Assets/Scripts/DIfficulty/Waves/LocateWave.cs
@@ -8,12 +8,11 @@ public class LocateWave : Wave
     private GridGenerator grid;
 
     [SerializeField]
-    private int locationSize = 2;
-
-    [SerializeField]
     private float startTime, dangerDelay;
 
     private Coroutine locateCor;
+
+    private List<Vector2Int> locations = new List<Vector2Int>();
 
     public override void StartWave() {
         locateCor = StartCoroutine(LocationMover()); 
@@ -28,22 +27,23 @@ public class LocateWave : Wave
 
     private IEnumerator LocationMover() {
         while(true) {
-            yield return new WaitForSeconds(startTime);
+            CreateNextLocation();
+            dangerCreator.SetAllDangerousWithException(locations, startTime);
+            yield return new WaitForSeconds(startTime + dangerDelay);
 
-            yield return new WaitForSeconds(dangerDelay);
             dangerCreator.SetAllSaferous();
+            yield return new WaitForSeconds(2f); // For anims
         }
     }
 
-    private Vector2Int ChooseNextPosition() {
-        // var current = road[road.Count-1];
-        // var possible = GetAdjacent(current);
-        // var chosen = possible.ElementAt(Random.Range(0, possible.Count-1));
-        // return chosen;
-        return Vector2Int.zero;
+    private void CreateNextLocation() {
+        locations.Clear();
+        var center = new Vector2Int(Random.Range(0, grid.Size.x-1), Random.Range(0, grid.Size.y-1));
+        locations.Add(center);
+        locations.AddRange(GetAround(center));
     }
 
-    private List<Vector2Int> GetAdjacent(Vector2Int pos) {
+    private List<Vector2Int> GetAround(Vector2Int pos) {
         var adjacent = new List<Vector2Int>();
         
         //TODO Ugly!!
@@ -59,10 +59,22 @@ public class LocateWave : Wave
         current = new Vector2Int(pos.x, pos.y+1);
         if(CheckValidRoadPiece(current)) adjacent.Add(current);
 
+        current = new Vector2Int(pos.x+1, pos.y+1);
+        if(CheckValidRoadPiece(current)) adjacent.Add(current);
+
+        current = new Vector2Int(pos.x-1, pos.y-1);
+        if(CheckValidRoadPiece(current)) adjacent.Add(current);
+
+        current = new Vector2Int(pos.x-1, pos.y+1);
+        if(CheckValidRoadPiece(current)) adjacent.Add(current);
+
+        current = new Vector2Int(pos.x+1, pos.y-1);
+        if(CheckValidRoadPiece(current)) adjacent.Add(current);
+
         return adjacent;
     }
 
     private bool CheckValidRoadPiece(Vector2Int pos) {
-        return (pos.x >= 0 && pos.x < grid.Size.x && pos.y >= 0 && pos.y < grid.Size.y );//&& !road.Contains(pos)
+        return (pos.x >= 0 && pos.x < grid.Size.x && pos.y >= 0 && pos.y < grid.Size.y && !locations.Contains(pos));
     }
 }
